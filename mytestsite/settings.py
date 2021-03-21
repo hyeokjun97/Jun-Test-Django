b6c462a7cd59e33f3fa9788ca5d4870c4d221980
Hyeok Jun Kim <hyeokjun97@gmail.com>
2021-03-21 14:46:19 +0900

    잘한 삽질

--- mytestsite/settings.py
+++ mytestsite/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog',
 ]
 
 MIDDLEWARE = [
@@ -50,7 +51,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'mytestsite.urls'
+ROOT_URLCONF = 'blog.urls'
 
 TEMPLATES = [
     {
